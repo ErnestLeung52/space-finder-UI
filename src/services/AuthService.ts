c3a580ae35eb1e7951874b53f0743cc648c12f2a
Ernest Leung <ernestleung52@gmail.com>
2023-05-14 01:00:53 -0700

    Access deny error with S3 put

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,5 +1,7 @@
 import { type CognitoUser } from '@aws-amplify/auth';
 import { Amplify, Auth } from 'aws-amplify';
+import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
 
 // Need IDs from AuthStack from Backend
 import { AuthStack } from '../../../space-finder/output.json';
@@ -18,11 +20,16 @@ Amplify.configure({
 });
 
 export class AuthService {
+	// Lazy initialization: all data inside authService(data service) will either have the states of object or undefined. When try to access it, if it's undefined, we will initialize it.
 	private user: CognitoUser | undefined;
+	private jwtToken: string | undefined;
+	private temporaryCredentials: object | undefined;
 
 	public async login(userName: string, password: string): Promise<object | undefined> {
 		try {
 			this.user = (await Auth.signIn(userName, password)) as CognitoUser;
+			// Need jwt to generate temp credentials
+			this.jwtToken = this.user?.getSignInUserSession()?.getIdToken().getJwtToken();
 			return this.user;
 		} catch (error) {
 			console.error(error);
@@ -30,7 +37,40 @@ export class AuthService {
 		}
 	}
 
+	public async getTemporaryCredentials() {
+		if (this.temporaryCredentials) {
+			return this.temporaryCredentials;
+		}
+
+		this.temporaryCredentials = await this.generateTemporaryCredentials();
+
+		return this.temporaryCredentials;
+	}
+
 	public getUserName() {
 		return this.user?.getUsername();
+	}
+
+	// Retrieve ideneityId, accessKeyId, secretAccessKeyId, sessionToken... from cognito
+	private async generateTemporaryCredentials() {
+		const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/${AuthStack.SpaceUserPoolId}`;
+
+		const cognitoIdentity = new CognitoIdentityClient({
+			credentials: fromCognitoIdentityPool({
+				// Backend has region stored as global, client-side needs configure
+				clientConfig: {
+					region: awsRegion,
+				},
+				identityPoolId: AuthStack.SpaceIdentityPoolId,
+				logins: {
+					// [cognitoIdentityPool]: this.jwtToken!
+					[cognitoIdentityPool]: this.jwtToken ?? '',
+				},
+			}),
+		});
+
+		const credentials = await cognitoIdentity.config.credentials();
+
+		return credentials;
 	}
 }
--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,8 +1,62 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { AuthService } from './AuthService';
+import { DataStack } from '../../../space-finder/output.json';
+
 export class DataService {
-	// public async createSpace(name: string, location: string, photo?: File) {
+	private authService: AuthService;
+	private s3Client: S3Client | undefined;
+	private awsRegion = 'us-west-1';
+
+	constructor(authService: AuthService) {
+		this.authService = authService;
+	}
+
 	public async createSpace(name: string, location: string, photo?: File) {
-		console.log(name, location, photo);
+		console.log('calling create space!!');
+		// Retrieve ideneityId, accessKeyId, secretAccessKeyId, sessionToken... from cognito, in order to do any AWS SDK action that the authenticated roles allow us
+
+		if (photo) {
+			const uploadUrl = await this.uploadPublicFile(photo);
+			console.log(uploadUrl);
+		}
+
 		return '123';
+	}
+
+	private async uploadPublicFile(file: File) {
+		const credentials = await this.authService.getTemporaryCredentials();
+
+		console.log(0);
+		console.log(credentials);
+
+		// Lazy init
+		if (!this.s3Client) {
+			console.log('????????');
+			this.s3Client = new S3Client({
+				credentials: credentials as any,
+				region: this.awsRegion,
+			});
+		}
+
+		console.log(1);
+
+		console.log(this.s3Client);
+
+		// Need bucket from the output of CDK deployment -> store file into bucket
+		const command = new PutObjectCommand({
+			Bucket: DataStack.SpaceFinderPhotosBucketName,
+			Key: file.name,
+			ACL: 'public-read',
+			Body: file,
+		});
+
+		console.log(2);
+
+		await this.s3Client.send(command);
+
+		console.log(3);
+		// URL for the newly uploaded file
+		return `https://${command.input.Bucket}.s3.${this.awsRegion}.amazonaws.com/${command.input.Key}`;
 	}
 
 	public isAuthorized() {
