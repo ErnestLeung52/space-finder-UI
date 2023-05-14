71c60d757a878aeb8824df846d6bac489abced0a
Ernest Leung <ernestleung52@gmail.com>
2023-05-13 17:19:38 -0700

    Added AuthService for login with amplify; created basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import { useState } from 'react';
+import './App.css';
+import LoginComponent from './components/LoginComponent';
+import { AuthService } from './services/AuthService';
+
+const authService = new AuthService();
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [userName, setUserName] = useState<string | undefined>(undefined);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const router = createBrowserRouter([
+		{
+			element: (
+				<>
+					<NavBar userName={userName} />
+					<Outlet />
+				</>
+			),
+			children: [
+				{
+					path: '/',
+					element: <div>Hello world!</div>,
+				},
+				{
+					path: '/login',
+					element: <LoginComponent authService={authService} setUserNameCb={setUserName} />,
+				},
+				{
+					path: '/profile',
+					element: <div>Profile page</div>,
+				},
+				{
+					path: '/createSpace',
+					element: <div>Create space page</div>,
+				},
+				{
+					path: '/spaces',
+					element: <div>Spaces page </div>,
+				},
+			],
+		},
+	]);
+
+	return (
+		<div className='wrapper'>
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/services/AuthService.ts
@@ -0,0 +1,36 @@
+import { type CognitoUser } from '@aws-amplify/auth';
+import { Amplify, Auth } from 'aws-amplify';
+
+// Need IDs from AuthStack from Backend
+import { AuthStack } from '../../../space-finder/output.json';
+
+const awsRegion = 'us-west-1';
+
+Amplify.configure({
+	Auth: {
+		mandatorySignIn: false,
+		region: awsRegion,
+		userPoolId: AuthStack.SpaceUserPoolId,
+		userPoolWebClientId: AuthStack.SpaceUserPoolClientId,
+		identityPoolId: AuthStack.SpaceIdentityPoolId,
+		authenticationFlowType: 'USER_PASSWORD_AUTH',
+	},
+});
+
+export class AuthService {
+	private user: CognitoUser | undefined;
+
+	public async login(userName: string, password: string): Promise<object | undefined> {
+		try {
+			this.user = (await Auth.signIn(userName, password)) as CognitoUser;
+			return this.user;
+		} catch (error) {
+			console.error(error);
+			return undefined;
+		}
+	}
+
+	public getUserName() {
+		return this.user?.getUsername();
+	}
+}
